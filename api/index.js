370d24185e3acccf52f81585ff2b779e197fbbf4
Waqas Ahmad Khan <24pwbcs1106@uetpeshawar.edu.pk>
2026-08-19 22:21:44 +0500

    Add root route and static file serving

--- api/index.js
+++ api/index.js
@@ -11,9 +11,34 @@ app.use(express.json());
 // Serve static files from public folder
 app.use(express.static('public'));
 
-// Import API routes
+// Route for home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+// Route for items page
+app.get('/items.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'items.html'));
+});
+
+// Route for form page
+app.get('/form.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'form.html'));
+});
+
+// Route for details page
+app.get('/details.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'details.html'));
+});
+
+// Route for about page
+app.get('/about.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'about.html'));
+});
+
+// API routes
 const itemsRouter = require('./items');
 app.use('/api/items', itemsRouter);
 
-// Export for Vercel (NO app.listen())
+// Export for Vercel
 module.exports = app;
